9bbdc76d12beca662ea4c41d29d89dbb47eab0a8
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2021-12-29 21:17:38 +0200

    Add files via upload

--- app.cpp
+++ app.cpp
@@ -1,3 +1,6 @@
+/* USER: MariosMitsios
+LANG: C++
+TASK: voting */
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -6,23 +9,23 @@
 #include <algorithm>
 
 // Classes
-class Candidate{
-    public:
-        Candidate(int candidateId){
-            _id = candidateId;
-        }
-        int getVotes(){
-            return _votes;
-        }
-        int getId(){
-            return _id;
-        };
-        void addVote(){
-            _votes++;
-        };
-    private:
-        int _id;
-        int _votes;
+class Candidate {
+public:
+    Candidate(int candidateId) {
+        _id = candidateId;
+    }
+    int getVotes() {
+        return _votes;
+    }
+    int getId() {
+        return _id;
+    };
+    void addVote() {
+        _votes++;
+    };
+private:
+    int _id;
+    int _votes;
 };
 
 // Variables
@@ -38,83 +41,95 @@ int votingData[4]; // 0: Total, 1: Voted, 2: Candidates, 3: Not Voted
 int passingCandidates;
 
 // Code
-std::vector<std::string> splitString(char splitWithCharacter, std::string stringIn){
+std::vector<std::string> splitString(char splitWithCharacter, std::string stringIn) {
     // Split string into segments
     std::string tmpData;
     std::stringstream StringStream(stringIn);
     std::vector<std::string> returnStrings;
-    
-    while(std::getline(StringStream, tmpData, splitWithCharacter)){
+
+    while (std::getline(StringStream, tmpData, splitWithCharacter)) {
         returnStrings.push_back(tmpData);
     }
 
     return returnStrings;
 }
 
-void writeFile(std::string fileName, int data){
+void writeFile(std::string fileName, int data) {
     std::ofstream file;
     file.open(fileName);
     file << data << "\n";
     file.close();
 }
 
-void declareCandidate(){
+void declareCandidate() {
     std::vector<int> allCandidateVotes;
-    for (Candidate i : candidates){
+    for (Candidate i : candidates) {
         allCandidateVotes.push_back(i.getVotes());
     }
 
     // Get max vote
     int maxVoted = *max_element(allCandidateVotes.begin(), allCandidateVotes.end());
 
-    if(votingData[0] != votingData[1]){
-        for (int i : allCandidateVotes){
-            if(i + votingData[3] > maxVoted){
+    if (votingData[0] != votingData[1]) {
+        for (int i : allCandidateVotes) {
+            if (i + votingData[3] > maxVoted) {
                 passingCandidates++;
             }
         }
     }
 }
 
-void countVotes(){
+void countVotes() {
     // Add votes to candidate
-    for (auto i : totalVotes){
-        for (Candidate ii : candidates){
-            if (ii.getId() == std::stoi(i) - 1){
-                ii.addVote();
-                candidates[ii.getId()] = ii;
+    for (auto i : totalVotes) {
+        for (Candidate ii : candidates) {
+            try
+            {
+                if (ii.getId() == std::stoi(i) - 1) {
+                    ii.addVote();
+                    candidates[ii.getId()] = ii;
+                }
             }
+            catch (const std::exception& e)
+            {
+            }
+
         }
     }
 }
 
-void addCandidates(){
+void addCandidates() {
     // Add candidates
-    for(int i; i < votingData[2]; i++){
-        candidates.push_back(Candidate(i));
+    try
+    {
+        for (int i; i < votingData[2]; i++) {
+            candidates.push_back(Candidate(i));
+        }
     }
+    catch (const std::exception& e)
+    {
+    }
+
 }
 
-void processData(){
+void processData() {
     // Split strings
     parameters[1] = splitString(' ', strings[1]);
     parameters[0] = splitString(' ', strings[0]);
 
     // Pass parameters from split strings to variables
     votingData[0] = std::stoi(parameters[1][0]);
-    votingData[1] = std::stoi(parameters[1][4]);
-    votingData[2] = std::stoi(parameters[1][2]);
+    votingData[2] = std::stoi(parameters[1][1]);
+    votingData[1] = std::stoi(parameters[1][2]);
     votingData[3] = votingData[0] - votingData[1];
 
     // Add all votes that are specified
-    for (int i; i < votingData[1] * 2; i++) {
-        if(i % 2 == 0){
-            totalVotes.push_back(parameters[0][i]);
-        }
+    for (int i; i < votingData[1]; i++) {
+        totalVotes.push_back(parameters[0][i]);
     }
 }
 
-void readFile(std::string file){
+void readFile(std::string file) {
     // Open file, read to end and save line data
     std::ifstream fileOpen(file);
     std::getline(fileOpen, strings[1]);
@@ -122,11 +137,13 @@ void readFile(std::string file){
     fileOpen.close();
 }
 
-int main(){
+int main() {
     readFile("voting.in");
     processData();
-    addCandidates();
-    countVotes();
-    declareCandidate();
-    writeFile("voting.out", passingCandidates);
+    if (votingData[1] <= votingData[0] && votingData[0] <= 1000000 && votingData[1] >= 1 && votingData[0] >= 1 && votingData[2] >= 1 && votingData[2] <= 1000) {
+        addCandidates();
+        countVotes();
+        declareCandidate();
+        writeFile("voting.out", passingCandidates);
+    }
 }
